815a9c4a09cf27738faf20719d1bf2c9006d1197
giangr40 <alessanro.giangreco@posteitaliane.it>
2020-01-03 12:35:26 +0100

    Preparation Code ForHandling Indescritta Files

--- src/main/java/org/giangro/sfi/genfileaffidoaders/App.java
+++ src/main/java/org/giangro/sfi/genfileaffidoaders/App.java
@@ -13,7 +13,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.io.File;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 /**
  *
@@ -26,44 +28,77 @@ import java.io.File;
 @PropertySource("classpath:gen_adersfiles_indescr.properties")
 public class App {
 
-  final static Logger logger
-    = LoggerFactory.getLogger(App.class);
+    final static Logger logger
+            = LoggerFactory.getLogger(App.class);
 
-  public static void main(String[] args) {
-    logger.info("=================================================");
-    logger.debug("Initializing spring framework");
-    AnnotationConfigApplicationContext ctx = null;
-    try {
-      ctx = new AnnotationConfigApplicationContext(App.class);
-      App app = ctx.getBean(App.class);
-      app.doStart();
-    } // try
-    finally {
-      if (ctx!=null) {
-        ctx.close();
-        logger.info("exit");
-      } // if
-    } // finally
+    final static String FILE_TYPE_DESCRITTA = "D";
+    final static String FILE_TYPE_INDESCRITTA = "I";
+    final static String FILE_TYPE_DEFAULT = FILE_TYPE_DESCRITTA;
 
-  }
+    public static void main(String[] args) {
+        logger.info("=================================================");
+        logger.debug("Initializing spring framework");
+        AnnotationConfigApplicationContext ctx = null;
+        try {
+            ctx = new AnnotationConfigApplicationContext(App.class);
+            App app = ctx.getBean(App.class);
+            app.doStart(args);
+        } // try
+        finally {
+            if (ctx != null) {
+                ctx.close();
+                logger.info("exit");
+            } // if
+        } // finally
 
-  private void doStart() {
-    logger.info("Starting ADERS File Generation Tool version "+this.version);   
-    try {
-                
-        this.genAderRSFilesDescritta
-                .generate();
-        
-    } // try
-    catch (Exception ex) {
-      logger.error(ex.getLocalizedMessage());
-    } // catch
-  }
-  
-  @Value("${version}")
-  private String version;
-  
-  @Autowired
-  private GenAdeRSFilesDescritta genAderRSFilesDescritta;
+    }
+
+    private void doStart(String[] args) {
+
+        final CmdLineParser parser = new CmdLineParser(this);
+
+        try {
+            parser.parseArgument(args);
+
+        } catch (CmdLineException clEx) {
+            logger.error("ERROR: Unable to parse command-line options: " + clEx);
+            System.exit(-1);
+        }
+
+        if (type == null) {
+            logger.info("setting file type to default: " + FILE_TYPE_DEFAULT);
+            type = FILE_TYPE_DEFAULT;
+        } // if
+
+        logger.info("Starting ADERS File Generation Tool version " + this.version);
+        logger.debug("file type: " + type);
+
+        try {
+
+            if (FILE_TYPE_INDESCRITTA.equals(type)) {
+                this.genAderRSFilesIndescritta
+                        .generate();
+            } //if
+            else {
+                this.genAderRSFilesDescritta
+                        .generate();
+            } // else 
+        } // try
+        catch (Exception ex) {
+            logger.error(ex.getLocalizedMessage());
+        } // catch
+    }
+
+    @Option(name = "-t", aliases = "--type", usage = "D=descritta,I=indescritta", required = false)
+    private String type;
+
+    @Value("${version}")
+    private String version;
+
+    @Autowired
+    private GenAdeRSFilesDescritta genAderRSFilesDescritta;
+    
+    @Autowired
+    private GenAdeRSFilesIndescritta genAderRSFilesIndescritta;
 
 } // class App
---
+++ src/main/java/org/giangro/sfi/genfileaffidoaders/GenAdeRSFilesIndescritta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.giangro.sfi.genfileaffidoaders;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author GIANGR40
+ */
+@Service
+public class GenAdeRSFilesIndescritta extends GenAdeRSFiles {
+
+    @Override
+    public void generate() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+} // GenAdeRSFilesIndescritta
